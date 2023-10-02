0c9a49094ecf7714671d05e2c355b808a6c25f53
Joshua-Rex <joshua.porterlightwood@gmail.com>
2023-10-02 16:41:46 +0100

    Trying to get the logo to work

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-import logo from './logo.svg';
 import './App.css';
 import Banner from "./components/banner";
 
--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -8,7 +8,7 @@ const subtitleStyle = {
     return (
       <header className="row mb-4">
         <div className="col-5">
-          <img src="logo.png" alt="logo"/>
+          <img src="./logo.png" alt="logo"/>
         </div>
         <div className="col-7 mt-5" style={subtitleStyle}>
           {children}
