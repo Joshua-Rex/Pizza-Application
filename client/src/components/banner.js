72725899262a00b3f89223310a58fcc367a6df63
Joshua-Rex <joshua.porterlightwood@gmail.com>
2023-10-03 14:36:38 +0100

    Successfully reverted the files back to a safe commit.

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -7,8 +7,9 @@ const subtitleStyle = {
   const Banner = ({ children }) => {
     return (
       <header className="row mb-4">
-        <div className="col-5">
-          <img src="./logo.png" alt="logo"/>
+        <div className="col-5 mt-5">
+          {/*<img src="logo.png" alt="logo"/>*/}
+          <p>Just imagine this is an image for the time being</p>
         </div>
         <div className="col-7 mt-5" style={subtitleStyle}>
           {children}
