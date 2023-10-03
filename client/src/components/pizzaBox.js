28e6f84edb1f739bff5e5f800045d58d5b34c974
Joshua-Rex <joshua.porterlightwood@gmail.com>
2023-10-03 14:32:32 +0100

    Revert "Preparing for a revert."
    This reverts commit 980a805f48d9322e59720ab1f5c4eda272450de0.

--- client/src/components/pizzaBox.js
+++ client/src/components/pizzaBox.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-const pizzaSize = [{name: "Small (8\")", price: 0}, {name: "Medium (12\")", price: 2.00}, {name: "Large(16\")", price: 4.00}, {name: "Pizzanormous (20\")", price: 8.00}];
-
+const pizzaSize = ["Small", "Medium", "Big", "Pizzanormous"];
 
 const PizzaBox = ( {pizza} ) => {
     const [sizes, setSizes] = useState(pizzaSize);
