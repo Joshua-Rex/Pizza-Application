d5a76785295271376daccb8a70c0dc4dcea39fb0
Joshua-Rex <joshua.porterlightwood@gmail.com>
2023-10-03 14:28:26 +0100

    Revert "Updated the components, added Pizza Box which includes all the information for the pizza's and Pizza List which contains all the pizza boxes."
    This reverts commit ee66fa8f36cc53229d11c0c570d38952fbb76a05.

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import Banner from "./components/banner";
+
+function App() {
+  {/*
+  const [data, setData] = React.useState(null);
+  const [pizzaBox, setPizzaBox] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+*/}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Banner>
+          <div>The Best Pizza Place Around</div>
+        </Banner>
+      </header>
+      {/* <div className="normalPizza">
+          <div className="pizzaBox">
+            <img className="pizzaImg" />
+            <h2 className="pizzaName">Demo Pizza 1</h2>
+            <p className="pizzaCal">2202</p>
+            <select className="pizzaSizes">
+              <option className="smallSize">Small</option>
+              <option className="mediumSize">Medium</option>
+              <option className="bigSize">Big</option>
+              <option className="superBigSize">Pizza Big</option>
+            </select>
+            <button className="customiseButton">Customise</button>
+          </div>
+        </div> */}
+        <div id="normalPizza">
+          {/*!pizzaBox ? <p>"Page Loading!"</p> : pizzaBox*/}
+        </div>
+        <p>
+          {/*!data ? "Loading..." : data*/ "Loading..."}
+        </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/banner.js
@@ -0,0 +1,20 @@
+const subtitleStyle = {
+    fontStyle: "italic",
+    fontSize: "x-large",
+    color: "coral",
+  };
+  
+  const Banner = ({ children }) => {
+    return (
+      <header className="row mb-4">
+        <div className="col-5">
+          <img src="./logo.png" alt="logo"/>
+        </div>
+        <div className="col-7 mt-5" style={subtitleStyle}>
+          {children}
+        </div>
+      </header>
+    );
+  };
+  
+  export default Banner;
